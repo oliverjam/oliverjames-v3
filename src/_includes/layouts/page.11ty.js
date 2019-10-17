2144706b54cdf57481be5242568e11a4c362d775
oliverjam <oliverjamesphillips@gmail.com>
2019-10-17 23:11:15 +0100

    Create generic page layout

---
+++ src/_includes/layouts/page.11ty.js
@@ -0,0 +1,40 @@
+const html = String.raw;
+
+module.exports = data => {
+  return html`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Oliver</title>
+        <link rel="stylesheet" href="/assets/css/global.css" />
+        <link rel="stylesheet" href="/assets/css/page.css" />
+      </head>
+      <body>
+        <header class="site-header">
+          <div>O</div>
+          <nav>
+            <ul>
+              <li><a href="/about">About</a></li>
+              <li><a href="/blog">Blog</a></li>
+              <li><a href="https://twitter.com/_oliverjam">Twitter</a></li>
+              <li><a href="https://github.com/oliverjam/">Github</a></li>
+              <li>
+                <a href="https://www.linkedin.com/in/oliverjam">LinkedIn</a>
+              </li>
+              <li>
+                <a href="mailto:hello@oliveram.es" title="hello@oliveram.es"
+                  >Email</a
+                >
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>${data.content}</main>
+        <footer class="site-footer">Footer</footer>
+      </body>
+    </html>
+  `;
+};
--- src/index.11ty.js
+++ src/index.11ty.js
@@ -1,24 +1,35 @@
-const head = require("./utils/head");
+const { relativeTime } = require("./utils/dates");
+
 const html = String.raw;
 
-module.exports = data => {
-  return html`
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        ${head()}
-        <title>Oliver</title>
-      </head>
-      <body>
-        ${data.collections.blog.reduce(
+class Home {
+  data() {
+    return {
+      layout: "layouts/page.11ty.js",
+    };
+  }
+  render(data) {
+    return html`
+      <div class="page-title">
+        <header>
+          <h1>I design and develop user experiences.</h1>
+        </header>
+      </div>
+      <ul style="display: grid; gap: 1rem;">
+        ${data.collections.blog.slice(-3).reduceRight(
           (acc, post) =>
             acc +
             html`
-              <a href=${post.url}>${post.data.title || post.fileSlug}</a>
+              <li style="display: grid; gap: 0.5rem;">
+                <a href=${post.url}>${post.data.title || post.fileSlug}</a>
+                ${relativeTime(post.date)}
+              </li>
             `,
           ""
         )}
-      </body>
-    </html>
-  `;
-};
+      </ul>
+    `;
+  }
+}
+
+module.exports = Home;
