2464b033604edabb0a6dfd12a4ce58a7cfc95070
oliverjam <oliverjamesphillips@gmail.com>
2019-10-17 23:11:25 +0100

    Create blog placeholder page

---
+++ src/blog.11ty.js
@@ -0,0 +1,28 @@
+const html = String.raw;
+
+class Blog {
+  data() {
+    return {
+      layout: "layouts/page.11ty.js",
+    };
+  }
+  render(data) {
+    return html`
+      <h1>Blog posts</h1>
+      <ul>
+        ${data.collections.blog.reduceRight(
+          (acc, post) =>
+            acc +
+            html`
+              <li>
+                <a href=${post.url}>${post.data.title || post.fileSlug}</a>
+              </li>
+            `,
+          ""
+        )}
+      </ul>
+    `;
+  }
+}
+
+module.exports = Blog;
