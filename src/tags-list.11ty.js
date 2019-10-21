682cc4ba4bbc34f66e3da0305e94af798672ca46
oliverjam <oliverjamesphillips@gmail.com>
2019-10-21 11:54:35 +0100

    Add initial tags pages

---
+++ src/tags-list.11ty.js
@@ -0,0 +1,30 @@
+const html = String.raw;
+
+class TagsList {
+  data() {
+    return {
+      layout: "layouts/default.11ty.js",
+      permalink: "/blog/tags/index.html",
+    };
+  }
+  render(data) {
+    const allTags = Object.keys(data.collections).filter(
+      key => !["all", "blog"].includes(key)
+    );
+    return html`
+      <h1>Tags</h1>
+      <ul>
+        ${allTags
+          .map(
+            tag =>
+              html`
+                <li><a href="${tag}">${tag}</a></li>
+              `
+          )
+          .join("\n")}
+      </ul>
+    `;
+  }
+}
+
+module.exports = TagsList;
---
+++ src/tags.11ty.js
@@ -0,0 +1,34 @@
+const html = String.raw;
+
+class TagsTemplate {
+  data() {
+    return {
+      layout: "layouts/default.11ty.js",
+      pagination: {
+        data: "collections",
+        size: 1,
+        alias: "tag",
+        filter: ["all", "blog"],
+      },
+      permalink: data => `/blog/tags/${data.tag}/`,
+    };
+  }
+  render(data) {
+    const postList = data.collections[data.tag];
+    return html`
+      <h1>${data.tag}</h1>
+      <ul>
+        ${postList
+          .map(
+            post =>
+              html`
+                <li><a href="${post.url}">${post.data.title}</a></li>
+              `
+          )
+          .join("\n")}
+      </ul>
+    `;
+  }
+}
+
+module.exports = TagsTemplate;
