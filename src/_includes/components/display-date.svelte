339515ac8d4271040c9afd3542a4d57b5687412a
oliverjam <oliverjamesphillips@gmail.com>
2020-05-24 15:22:14 +0100

    Add Svelte helper components
    For tags and dates

---
+++ src/_includes/components/display-date.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { formatDate } from "../../utils/dates";
+  export let date;
+  const formattedDate = formatDate(date);
+</script>
+
+<time datetime={date.toISOString()} title={date}>{formattedDate}</time>
---
+++ src/_includes/components/tags-list.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Tag from "./tag.svelte";
+
+  export let tags;
+</script>
+
+<ul class="cluster" style="--space: 2rem; justify-content: center;">
+  {#each tags as [tag, matchingPosts]}
+    <li>
+      <Tag {tag} matches={matchingPosts.length} />
+    </li>
+  {/each}
+</ul>
