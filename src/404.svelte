d490c18bf263e1c9d1bce78ea35bb21f1ef017c0
oliverjam <oliverjamesphillips@gmail.com>
2020-05-24 14:56:23 +0100

    Rewrite 404 as Svelte template

---
+++ src/404.svelte
@@ -0,0 +1,9 @@
+<script context="module">
+  export const data = {
+    layout: "layouts/default.11ty.js",
+    permalink: "blog/tags/404.html",
+    title: "Page not found"
+  };
+</script>
+
+<h1>Post not found</h1>
