5a6e62453caab769f46bb8c2c6d5b0d4aae26b31
oliverjam <oliverjamesphillips@gmail.com>
2020-05-24 15:32:26 +0100

    Convert blog page to Svelte

---
+++ src/blog.svelte
@@ -0,0 +1,28 @@
+<script context="module">
+  export const data = {
+    layout: "layouts/default.11ty.js",
+    title: "All posts"
+  };
+</script>
+
+<script>
+  import { getAllTags } from "./utils/tags";
+  import PostsList from "./_includes/components/posts-list.svelte";
+  import Tag from "./_includes/components/tag.svelte";
+
+  export let data;
+  const allTags = getAllTags(data.collections);
+  const tags = allTags.slice(0, 3);
+  const posts = data.collections.blog;
+</script>
+
+<h1>All posts</h1>
+
+<nav aria-label="categories" class="cluster" style="justify-content: center;">
+  {#each tags as [tag, matchingPosts]}
+    <Tag {tag} matches={matchingPosts.length} size="small" />
+  {/each}
+  <a href="tags" class="tag">All tags</a>
+</nav>
+
+<PostsList {posts} />
